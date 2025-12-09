62e48bf04b01282a57ddce6a8ec09ccad7340039
Sarvesh <sarveshvaradkar123@gmail.com>
2025-12-09 23:49:18 +0530

    Repository added

---
+++ internal/repo/repo.go
@@ -0,0 +1,37 @@
+package repo
+
+import (
+	"context"
+	"errors"
+
+	"github.com/SARVESHVARADKAR123/task-manager-cli/internal/domain"
+)
+
+
+var (
+	ErrNotFound	= errors.New("task not found")
+	ErrConflict	= errors.New("task conflict")
+	ErrInternal = errors.New("internal repository error")
+)
+
+type TaskRepository interface {
+	Create(ctx context.Context, t *domain.Task) error
+	Update(ctx context.Context, t *domain.Task) error
+	Get(ctx context.Context, id domain.TaskID) (*domain.Task, error)
+	Delete(ctx context.Context, id domain.TaskID) error
+	List(ctx context.Context, filter domain.TaskFilter) ([]*domain.Task, error)
+}
+
+
+type FileRepoConfig struct {
+	Path string
+}
+
+type FileData struct{
+	Version int  			`json:"version"`
+	Tasks 	[]*domain.Task 	`json:"tasks"`
+}
+
+
+
+
--- internal/repo/task_repo.go
+++
@@ -1,9 +0,0 @@
-package repo
-
-import "github.com/SARVESHVARADKAR123/task-manager-cli/internal/domain"
-
-type TaskRepo interface {
-	Add(task domain.Task) error
-	List() ([]domain.Task, error)
-	Delete(id string) error
-} 
