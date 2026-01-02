72844f219aac5de5d0dd10ad8a9206569bcbb278
Sarvesh <sarveshvaradkar123@gmail.com>
2026-01-03 00:46:46 +0530

    service errors added

--- internal/app/service/task/errors.go
+++ internal/app/service/task/errors.go
@@ -1,1 +1,10 @@
 package task
+
+import "errors"
+
+var (
+	ErrInvalidInput  = errors.New("invalid input")
+	ErrAlreadyExists = errors.New("task already exists")
+	ErrNotFound      = errors.New("task not found")
+	ErrAlreadyDone   = errors.New("task already completed")
+)
