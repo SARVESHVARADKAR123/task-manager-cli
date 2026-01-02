124d1986a0dcbc01fb1cb2c78afaeac0abd959a6
Sarvesh <sarveshvaradkar123@gmail.com>
2026-01-03 00:45:47 +0530

    ports/interface added

--- internal/app/ports/clock.go
+++ internal/app/ports/clock.go
@@ -1,1 +1,9 @@
 package ports
+
+import "time"
+
+// Clock provides the current time.
+// Abstracted for testability and consistency.
+type Clock interface {
+	NowUTC() time.Time
+}
--- internal/app/ports/task_repository.go
+++ internal/app/ports/task_repository.go
@@ -1,1 +1,17 @@
 package ports
+
+import(
+	"context"
+	"github.com/SARVESHVARADKAR123/task-manager-cli/internal/domain"
+)
+
+type TaskRepository interface{
+	Create(ctx context.Context, task *domain.Task) error
+	Update(ctx context.Context,task *domain.Task) error
+	Get(ctx context.Context,id domain.TaskID) (*domain.Task,error)
+	Delete(ctx context.Context,id domain.TaskID) error
+	List(ctx context.Context,filter domain.TaskFilter) ([]*domain.Task,error)
+	
+
+}
+	
